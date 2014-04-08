6dc0586ab6273311f540f8565d44256471a0af87
John DeSilva <desilvjo@umich.edu>
2014-04-08 15:45:27 -0400

    Move navigation drawer fragment

--- hubski-android/src/main/java/com/aesthetikx/hubski/fragment/NavigationDrawerFragment.java
+++ hubski-android/src/main/java/com/aesthetikx/hubski/fragment/NavigationDrawerFragment.java
@@ -1,4 +1,4 @@
-package com.aesthetikx.hubski;
+package com.aesthetikx.hubski.fragment;
 
 import android.support.v7.app.ActionBarActivity;;
 import android.app.Activity;
@@ -21,6 +21,7 @@ import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 import android.widget.Toast;
+import com.aesthetikx.hubski.R;
 
 /**
  * Fragment used for managing interactions for and presentation of a navigation drawer.
