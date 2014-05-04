b386e335ddfbf5f0089043003acda6817b0281fa
John DeSilva <desilvjo@umich.edu>
2014-05-04 13:57:09 -0400

    Fix comment parser recursive bug

--- hubski-android/src/main/java/com/aesthetikx/hubski/parse/CommentParser.java
+++ hubski-android/src/main/java/com/aesthetikx/hubski/parse/CommentParser.java
@@ -14,20 +14,6 @@ import java.util.List;
 public class CommentParser {
 
     public static Comment parse(String html) throws MalformedURLException {
-        URL ul = new URL("http://www.google.com");
-        URL l = new URL("http://www.google.com");
-
-        List<Comment> rootChildren = new ArrayList<>();
-        for (int x = 0; x < 2; x++) {
-            List<Comment> xChildren = new ArrayList<>();
-            for (int y = 0; y < 2; y++) {
-                xChildren.add(new Comment("x" + x + "y" + y, ul, l, "body", "age", 1, new ArrayList<Comment>(), 2));
-            }
-            rootChildren.add(new Comment("x" + x, ul, l, "body", "age", 1, xChildren, 1));
-        }
-        Comment rootComment = new Comment("cat", ul, l, "body", "age", 1, rootChildren, 0);
-        return rootComment;
-        /*
         Document doc = Jsoup.parse(html);
 
         List<Comment> rootChildren = new ArrayList<>();
@@ -37,11 +23,10 @@ public class CommentParser {
         for (int i = 0; i < outercomms.size(); i++) {
             Element outercomm = outercomms.get(i);
             Element subcom = subcoms.get(i);
-            rootChildren.add(getComment(outercomm, subcom));
+            rootChildren.add(getComment(outercomm, subcom, 1));
         }
 
         return getRootComment(doc, rootChildren);
-        */
     }
 
     private static Comment getRootComment(Document doc, List<Comment> children) {
@@ -59,11 +44,10 @@ public class CommentParser {
         return new Comment("cat", userLink, link, body, age, score, children, 0);
     }
 
-    private static Comment getComment(Element outercomm, Element subcom) {
-        System.out.println("getcomment called");
-        int score = Integer.parseInt(outercomm.select("div.plusminus > span.score a > img")
+    private static Comment getComment(Element outerComm, Element subCom, int depth) {
+        int score = Integer.parseInt(outerComm.select("div.plusminus > span.score a > img")
                 .get(0).attr("class").substring(0, 1));
-        Element userElement = outercomm.select("div > span.subhead > span#username > a").get(0);
+        Element userElement = outerComm.select("div > span.subhead > span#username > a").get(0);
         String username = userElement.text();
         URL userLink = null;
         try {
@@ -73,32 +57,30 @@ public class CommentParser {
         }
         URL link = null;
         try {
-            link = new URL("http://www.hubski.com/" + outercomm.select("div > span.subhead > a").attr("href"));
+            link = new URL("http://www.hubski.com/" + outerComm.select("div > span.subhead > a").attr("href"));
         } catch (Exception e) {
             e.printStackTrace();
         }
-        String body = outercomm.select("div > div#comtext.comm").text();
+        String body = outerComm.select("div > div#comtext.comm").text();
         String age = "TODO";
 
-        List<Comment> children = parseSubcom(subcom);
-        System.out.println("getcomment returning");
-        return new Comment(username, userLink, link, body, age, score, children, 0);
+        Element subSubCom = subCom.select("div.subsubcom").first();
+        List<Comment> children = parseSubSubCom(subSubCom, depth + 1);
+        return new Comment(username, userLink, link, body, age, score, children, depth);
     }
 
-    private static List<Comment> parseSubcom(Element parentsubcom) {
-        System.out.println("Parse subcom called");
-        Elements outercomms = parentsubcom.select("div.subsubcom > div.outercomm");
-        Elements subcoms = parentsubcom.select("div.subsubcom > div.subcom");
+    private static List<Comment> parseSubSubCom(Element parentSubSubCom, int depth) {
+        Elements outerComms = parentSubSubCom.select("> div.outercomm");
+        Elements subComs = parentSubSubCom.select("> div.subcom");
 
         List<Comment> children = new ArrayList<>();
 
-        for (int i = 0; i < outercomms.size(); i++) {
-            Element outercomm = outercomms.get(i);
-            Element subcom = subcoms.get(i);
-            children.add(getComment(outercomm, subcom));
+        for (int i = 0; i < outerComms.size(); i++) {
+            Element outerComm = outerComms.get(i);
+            Element subCom = subComs.get(i);
+            children.add(getComment(outerComm, subCom, depth));
         }
 
-        System.out.println("Parse subcom returning with " + children.size() + " children");
         return children;
     }
 }
